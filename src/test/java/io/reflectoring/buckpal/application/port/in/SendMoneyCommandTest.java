4af42230e59d2459d83deda94adffbabd4a5676d
Shangxing Jiang <shangxing.jiang@thoughtworks.com>
2024-08-10 16:01:31 +0800

    test coverage change3

--- src/test/java/io/reflectoring/buckpal/application/port/in/SendMoneyCommandTest.java
+++ src/test/java/io/reflectoring/buckpal/application/port/in/SendMoneyCommandTest.java
@@ -19,24 +19,24 @@ class SendMoneyCommandTest {
         // no exception
     }
 
-//    @Test
-//    public void moneyValidationFails() {
-//        Assertions.assertThrows(ConstraintViolationException.class, () -> {
-//            new SendMoneyCommand(
-//                    new Account.AccountId(42L),
-//                    new Account.AccountId(43L),
-//                    new Money(new BigInteger("-10")));
-//        });
-//    }
+    @Test
+    public void moneyValidationFails() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            new SendMoneyCommand(
+                    new Account.AccountId(42L),
+                    new Account.AccountId(43L),
+                    new Money(new BigInteger("-10")));
+        });
+    }
 
-//    @Test
-//    public void accountIdValidationFails() {
-//        Assertions.assertThrows(ConstraintViolationException.class, () -> {
-//            new SendMoneyCommand(
-//                    new Account.AccountId(42L),
-//                    null,
-//                    new Money(new BigInteger("10")));
-//        });
-//    }
+    @Test
+    public void accountIdValidationFails() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            new SendMoneyCommand(
+                    new Account.AccountId(42L),
+                    null,
+                    new Money(new BigInteger("10")));
+        });
+    }
 
 }
